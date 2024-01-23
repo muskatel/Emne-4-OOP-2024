cb0a4251da4e8579c6195a28074fb44184d51e6a
Craig Marais <craig.marais.85@gmail.com>
2024-01-23 11:08:18 +0100

    Added Peoples Project

---
+++ Peoples/Peoples/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Peoples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person me = new Person("Craig Marais");
+        
+        me.update(new DateTime(1985,06,12));
+        
+        Console.WriteLine(me.getName());
+        Console.WriteLine(me.getAge());
+    }
+}
