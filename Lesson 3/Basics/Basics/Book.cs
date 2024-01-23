e274ad69ed0e2666c4c4d638ee0470b31b90ac7f
Craig Marais <craig.marais.85@gmail.com>
2024-01-23 10:27:53 +0100

    Added Basic Book example

---
+++ Lesson 3/Basics/Basics/Book.cs
@@ -0,0 +1,50 @@
+﻿namespace Basics;
+
+/// <summary>
+/// Craig's Cool Book Class written for class!
+/// </summary>
+public class Book
+{
+    /// <summary>
+    /// Book constructor
+    /// </summary>
+    /// <param name="name">The name of the book.</param>
+    /// <param name="author">The book's author.</param>
+    public Book(String name, String author)
+    {
+        this.name = name;
+        this.author = author;
+    }
+
+    /// <summary>
+    /// Prints a label for a book
+    /// </summary>
+    /// <returns>The authors name and then the book title as a compound string.</returns>
+    public String PrintLabel()
+    {
+        String label = "";
+        label += author + " - " + name;
+        return label;
+    }
+    
+    // variable, of type string, called name, that is private
+    private String name;
+    
+    // public getter for name
+    public String Name
+    {
+        get => name;
+        //set => name = value;
+    }
+
+    // variable, of type string, called author, that is private
+    private String author;
+
+    // public getter for author
+    public String Author
+    {
+        get => author;
+    }
+
+    
+}
---
+++ Lesson 3/Basics/Basics/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Basics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        // this name is only for me
+        Book tac = new Book("Thinking as Computation","H. Levesque");
+        //tac.Name = "Thinking as Computation";
+        //tac.author = "H. Levesque";
+
+        Book lotr1 = new Book("The Fellowship of the Ring", "J.R.R. Tolkien");
+        Book lotr2 = new Book("The Two Towers", "J.R.R. Tolkien");
+        Book lotr3 = new Book("Return of the King", "J.R.R. Tolkien");
+
+        List<Book> myBooks = new List<Book>();
+        myBooks.Add(lotr1);
+        myBooks.Add(lotr2);
+        myBooks.Add(lotr3);
+        */
+        
+        List<Book> myBooks = new List<Book>();
+        myBooks.Add(new Book("The Fellowship of the Ring", "J.R.R. Tolkien"));
+        myBooks.Add(new Book("The Two Towers", "J.R.R. Tolkien"));
+        myBooks.Add(new Book("Return of the King", "J.R.R. Tolkien"));
+        myBooks.Add(new Book("The Hobbit", "J.R.R. Tolkien"));
+        myBooks.Add(new Book("Thinking as Computation","H. Levesque"));
+
+        // how to find a book by name
+        // Console.WriteLine(myBooks.Find(book => book.Name == "The Hobbit").Name);
+        
+        
+        foreach (Book book in myBooks)
+        {
+            if (book.Name.Contains("Ring"))
+            {
+                Console.WriteLine("Found a \"ring\" book");
+                Console.WriteLine(book.PrintLabel());
+            }
+        }
+        
+    }
+}
