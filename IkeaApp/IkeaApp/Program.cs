80d25a26d6d1fbac96db49204d52c2987dee5a81
Craig Marais <craig.marais.85@gmail.com>
2024-02-12 10:15:18 +0100

    init ikea

---
+++ IkeaApp/IkeaApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IkeaApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, IKEA!");
+        
+        
+    }
+}
---
+++ RandomNumbers/RandomNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace RandomNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String[] names =
+        {
+            "Aldric Ironheart",
+            "Sylvia Moonshadow",
+            "Thrain Fireforge",
+            "Elowen Stormrider",
+            "Gideon Ravenwing",
+            "Isolde Lionheart",
+            "Bran Shadowblade",
+            "Elysia Starfall",
+            "Faelan Dragonbane",
+            "Seraphina Thunderfoot",
+            "Cedric Frostbeard",
+            "Lavinia Emberflame",
+            "Percival Stormcaller",
+            "Rowan Silverleaf",
+            "Morgana Frostshadow",
+            "Gareth Swiftwind",
+            "Rosalind Darkwater",
+            "Finnian Stonehelm",
+            "Celestia Skydancer",
+            "Alaric Blackthorn"
+        };
+        
+        
+        Console.WriteLine("Hello, World!");
+
+        Random random = new Random();
+        /*
+        for (int i = 0; i < 10; i++)
+        {
+           Console.WriteLine(random.Next(0,100));
+        }
+        */
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(names[random.Next(0, names.Length)]);
+        }
+    }
+}
