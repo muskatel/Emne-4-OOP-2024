60ab2db31c720cc121745b2551fd07fc8cfae2b7
Craig Marais <craig.marais.85@gmail.com>
2024-02-13 10:53:23 +0100

    delegates

---
+++ Weather/Tuples/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Tuples;
+
+class BodyStats : Tuple<double, double>
+{
+    public BodyStats(double height, double weight)
+        :base(height, weight)
+    {
+
+    }
+
+    public double Height
+    {
+        get
+        {
+            return this.Item1;
+        } 
+        
+    }
+    public double Weight { get{ return this.Item2; } }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        BodyStats stats = new BodyStats(175, 70);
+
+
+        List<(double, double)> stats2 = new List<(double Height, double Weight)>
+        {
+            (Height: 175, Weight: 70),
+            (Height:180, Weight: 85)
+        };
+
+
+    }
+}
---
+++ Weather/Weather2.0/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Weather2._0;
+
+class Program
+{
+    delegate bool Warning(double temperature, double humidity,ref string message);
+
+    static bool FreezingWarning(double temperature, double humidity, ref string message)
+    {
+        if (temperature <= 0)
+        {
+            message = "--- Freezing warning!!! ---";
+            return true;
+        }
+
+        return false;
+    }
+    
+    static bool RainWarning(double temperature, double humidity,ref string message)
+    {
+        if (humidity >=50)
+        {
+            message = "--- Rain warning!!! ---";
+            return true;
+        }
+
+        return false;
+    }
+    
+    static void Main(string[] args)
+    {
+        List<(double Temperature, double Humidity)> weatherConditions =
+        [
+            (20.0, 40.0),
+            (10.0, 50.0),
+            (20.0, 50.0),
+            (-4, 15),
+            (0, 10),
+            (15, 75)
+            
+        ];
+        weatherConditions.Add((20.0, 50.0));
+
+        List<Warning> warnings = new List<Warning>()
+        {
+            FreezingWarning,
+            RainWarning
+        };
+        
+
+        foreach ((double Temperature, double Humidity) condition in weatherConditions)
+        {
+            Console.WriteLine($"\nConditions \n" +
+                              $"Temp:{condition.Temperature}\n" +
+                              $"Humidity:{condition.Humidity}");
+            foreach (Warning warning in warnings)
+            {
+                string message = "";
+                if (warning(condition.Temperature, condition.Humidity, ref message))
+                {
+                    Console.WriteLine(message);
+                }
+            }
+
+        }
+
+    }
+}
