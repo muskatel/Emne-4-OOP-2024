d7883c2f83a7991ed33c6de24ae4607b71b17cb6
Craig Marais <craig.marais.85@gmail.com>
2024-03-11 10:39:08 +0100

    hello

---
+++ Birds/Program.cs
@@ -0,0 +1,45 @@
+﻿using Birds.Interfaces;
+using Birds.Species;
+
+namespace Birds;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Bird> birds = new List<Bird>();
+        
+        birds.Add(new Crow());
+        birds.Add(new Duck());
+        birds.Add(new Penguin());
+        birds.Add(new Thrush());
+
+
+        foreach (Bird bird in birds)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine(bird);
+            Console.WriteLine("-------------------------------------");
+            if (bird is IFly flybird)
+            {
+                flybird.Fly();
+            }
+            
+            if (bird is IFloat floatbird)
+            {
+               floatbird.Float();
+            }
+            
+            if (bird is INoise noisebird)
+            {
+                noisebird.Noise();
+            }
+            
+            if (bird is ISwim swimbird)
+            {
+               swimbird.Swim();
+            }
+            Console.WriteLine("-------------------------------------\n");
+        }
+    }
+}
---
+++ DBConnection/Program.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace DBConnection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MySqlConnection connection;
+        string server = "localhost"; // or IP
+        string database = "world";
+        string user = "root";
+        string password = "gokstad";
+        string port = "3306";
+        string sslM = "none";
+
+        String SelectCityString = $"SELECT * FROM city;";
+
+
+        MySqlCommand readCityCommand;
+        MySqlDataReader reader = null;
+        
+        
+
+        string connectionString =
+            $"server={server};port={port};user id={user}; password={password}; database={database}; SslMode={sslM}";
+
+        connection = new MySqlConnection(connectionString);
+
+        
+        try
+        {
+            connection.Open();
+
+            Console.WriteLine("Successfully connected");
+
+            readCityCommand = new MySqlCommand(SelectCityString, connection);
+            reader = readCityCommand.ExecuteReader();
+            
+            while(reader.Read())
+            {
+                Console.WriteLine($"{reader.GetInt32("ID")}: {reader.GetString("Name")}");
+            }
+            
+            Console.WriteLine("Done. Disconnecting");
+            connection.Close();
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine(ex.Message + connectionString);
+        }
+
+    }
+}
