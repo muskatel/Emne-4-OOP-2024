9bc154d714aa50e8bef37a111014ffab23c6a0fd
Craig Marais <craig.marais.85@gmail.com>
2024-04-16 09:05:50 +0200

    stuff

---
+++ ProjectTypes/ConsoleWithDLLImport/ConsoleWithDLLImport/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassLibraryInsideExisting;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+NumberCruncher nc = new NumberCruncher(3242, 7);
+Console.WriteLine(nc.Output());
---
+++ ProjectTypes/GradeAnalyzer/GradeAnalyzer/Grade.cs
@@ -0,0 +1,47 @@
+﻿namespace GradeAnalyzer;
+
+public class Grade
+{
+    public int value;
+
+
+    public string GradetoCharacter()
+    {
+        if (value >= 80)
+        {
+            return "A";
+        }
+        else if (value >= 70)
+        {
+            return "B";
+        }
+        else if (value >= 60)
+        {
+            return "C";
+        }
+        else if (value >= 50)
+        {
+            return "D";
+        }
+        else if (value >= 40)
+        {
+            return "E";
+        }
+        else
+        {
+            return "F";
+        }
+    }
+    
+    public string GradetoPF()
+    {
+        if (value > 50)
+        {
+            return "Pass";
+        }
+        else
+        {
+            return "Fail";
+        }
+    }
+}
